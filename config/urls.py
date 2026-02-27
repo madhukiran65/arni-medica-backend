73add8ddfa26f34a71e5691d5aaca7100458538c
MK <mkparvathaneni6@gmail.com>
2026-02-27 01:16:48 +0000

    Bump build marker to v19-feedback-fix

--- config/urls.py
+++ config/urls.py
@@ -31,7 +31,7 @@ def _db_check(request):
             mig_files[app_dir] = sorted([f for f in os.listdir(mig_path) if f.endswith('.py') and f != '__init__.py'])
     result['migration_files_on_disk'] = mig_files
     # Build version
-    result['build_marker'] = 'v18-feedback'
+    result['build_marker'] = 'v19-feedback-fix'
     return JsonResponse(result)
 
 
